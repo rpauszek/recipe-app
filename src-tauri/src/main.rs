b2b4b05c6430736dce6f76dff8cef505c8e21054
Ray Pauszek <ray.pauszek@gmail.com>
2025-10-26 12:09:08 +0100

    backend: set up dev recipes structure; load from json

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod recipes;
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::new().build())
+        .setup(|_app| {
+            // This runs before the window opens
+            let recipes_json = recipes::load_dev_recipes();
+            log::info!("Loaded dev recipes: {:#?}", recipes_json);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    recipe_app_lib::run()
 }
